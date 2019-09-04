c17d85231f5c88d70ed22c3a0a96cd3e779c37d5
loc.nguyen <loc.nguyen@orbium.com>
2019-09-04 13:56:13 +0200

    structure and dummy web service implemented

---
+++ src/app/components/clientview/clt-pos/clt-pos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clt-pos',
+  templateUrl: './clt-pos.component.html',
+  styleUrls: ['./clt-pos.component.css']
+})
+export class CltPosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
